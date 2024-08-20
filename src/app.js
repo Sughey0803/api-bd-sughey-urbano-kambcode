d6ba119e52473d687f4b95cc45bff267e896a7fa
Sughey <sughey_urbano@yahoo.es>
2024-08-19 22:03:01 -0500

    Agrega app.js configuración y sequelize dependencia

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const app = express();
+const studentsRoutes = require('./routes/studentRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+const enrollmentRoutes = require('./routes/enrollmentRoutes');
 
+const app = express();
+// Middleware para procesar JSON
 app.use(express.json());
 
+//Configuración de las rutas
+app.use('/students', studentRoutes);
+app.use('/course', courseRoutes);
+app.use('/enrollment', enrollmentRoutes);
+
+//Exportación del modulo app para que pueda ser usado en index.js
 module.exports = app;
